db3c162d98f098051689aa9803121b29158a1070
LucienBouletRoblin <lucien.bouletroblin@gmail.com>
2017-06-29 17:14:06 +0200

    fixing routing in angugu

--- frontEndTA/src/app/app.module.ts
+++ frontEndTA/src/app/app.module.ts
@@ -3,13 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule }    from '@angular/http';
 
-import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 
-import { RequestOptions } from '@angular/http';
-
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
